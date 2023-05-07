17c90a7ecd751adbb88ebed9dd3668e7fffb72b4
Jay <jpca999@gmail.com>
2023-05-07 10:14:45 -0400

    now i can iterte ovr json data without the findPath content variable

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -21,11 +21,13 @@ public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
 
-//    private static final String BASE_URL = "http://localhost:8080";
-//    private static final String GET_BEER_PATH = "/api/v1/beer";
+    private static final String BASE_URL = "http://localhost:8080"; private static final String GET_BEER_PATH = "/api/v1/beer";
 
-    private static final String BASE_URL = "http://api.springframework.guru/";
-    private static final String GET_BEER_PATH = "/api/v1/beer";
+//    private static final String BASE_URL = "http://api.springframework.guru/"; private static final String GET_BEER_PATH = "/api/v1/beer";
+
+
+//    http://localhost:8080/api/v1/beer
+//    private static final String BASE_URL = "https://dummyjson.com";    private static final String GET_BEER_PATH = "/products";
 
     @Override
     public Page<BeerDTO> listBeers() {
@@ -34,21 +36,23 @@ public class BeerClientImpl implements BeerClient {
         ResponseEntity<String> stringResponse =
                 restTemplate.getForEntity(BASE_URL + GET_BEER_PATH , String.class);
 
-        ResponseEntity<Map> mapResponse =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+//        ResponseEntity<Map> mapResponse =
+//                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
 
         ResponseEntity<JsonNode> jsonResponse =
                 restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, JsonNode.class);
 
         System.out.println("-------------37------------- = "  );
 
-        jsonResponse.getBody().findPath("content")
+//        jsonResponse.getBody().findPath("content")
+//                .elements().forEachRemaining(node -> {
+//                    System.out.println(node.get("beerName").asText());
+//                });
+
+        jsonResponse.getBody()
                 .elements().forEachRemaining(node -> {
                     System.out.println(node.get("beerName").asText());
                 });
-
-        System.out.println("-------------45------------- = "  );
-
 
         System.out.println(stringResponse.getBody());
 
