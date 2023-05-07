e679d443483a4825b58d22d4d4333f54cbc27373
Jay <jpca999@gmail.com>
2023-05-06 21:10:57 -0400

    195 now working after i changed the url from local host to  the api.springfrme.guru bla blal

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring6resttemplate.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -20,7 +21,10 @@ public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
 
-    private static final String BASE_URL = "http://localhost:8080";
+//    private static final String BASE_URL = "http://localhost:8080";
+//    private static final String GET_BEER_PATH = "/api/v1/beer";
+
+    private static final String BASE_URL = "http://api.springframework.guru/";
     private static final String GET_BEER_PATH = "/api/v1/beer";
 
     @Override
@@ -30,12 +34,25 @@ public class BeerClientImpl implements BeerClient {
         ResponseEntity<String> stringResponse =
                 restTemplate.getForEntity(BASE_URL + GET_BEER_PATH , String.class);
 
+        ResponseEntity<Map> mapResponse =
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+
         ResponseEntity<JsonNode> jsonResponse =
                 restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, JsonNode.class);
+
+        System.out.println("-------------37------------- = "  );
+
+        jsonResponse.getBody().findPath("content")
+                .elements().forEachRemaining(node -> {
+                    System.out.println(node.get("beerName").asText());
+                });
+
+        System.out.println("-------------45------------- = "  );
 
 
         System.out.println(stringResponse.getBody());
 
         return null;
+
     }
 }
